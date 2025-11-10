998bbec36e78452c87003e0eac3a19107431d2f7
Liam Hopkins <we.hopkins@gmail.com>
2025-11-09 18:10:10 -0800

    rewrite gallery

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,80 +1,59 @@
 import React from 'react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import { useDragSelect } from './DragSelectContext.jsx';
+import { RowsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/rows.css";
 
-
-function getRealImagePath(image) {
-  const url = new URL(image);
-  return decodeURI(url.pathname).substring(1);
+async function getImageDimensions(src) {
+  return new Promise(resolve => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      img.remove();
+      resolve({ width: img.naturalWidth, height: img.naturalHeight });
+    };
+    document.body.appendChild(img);
+  });
 }
 
-
-function Image({ src, onclick }) {
-  const ds = useDragSelect();
-  const imgEl = useRef(null);
-
-  useEffect(() => {
-    const element = imgEl.current;
-    if (!element || !ds) return;
-    ds.addSelectables(element);
-  }, [ds, imgEl]);
-
-  return (
-    <div className='item selectable' onClick={onclick}>
-      <img src={src} ref={imgEl} className='imgitem' />
-    </div>
-  );
+function getAllImages(images) {
+  const promises = images.map(async (image , index) => {
+    const dimensions = await getImageDimensions(image);
+    return {
+        src: image,
+        key: `gallery-item-${index}`,
+        ...dimensions,
+    };
+  });
+  return Promise.all(promises);
 }
 
-export default function Gallery({ images, selectedCount, setIndex, setSelectedImages }) {
+export default function Gallery({ images, selectedCount, setIndex }) {
   console.log('gallery render');
+  const [photos, setPhotos] = useState([]);
   const galleryRef = useRef(null);
 
-  const ds = useDragSelect();
+  console.log('gallery render: got photos:', photos);
 
-  // dragselect
   useEffect(() => {
-    console.log('gallery:useEffect: dragselect');
-    if (!ds) return;
-
-    const endId = ds.subscribe('DS:end', ({ items }) => {
-      const selected = items.map(({ src }) => {
-        return getRealImagePath(src);
-      });
-      setSelectedImages(selected);
+    getAllImages(images).then((i) => {
+      setPhotos(i);
     });
-
-    ds.setSettings({
-      area: galleryRef.current
-    });
-
-    return () => {
-      ds.unsubscribe('DS:end', null, endId);
-    }
-  }, [ds, setSelectedImages]);
-
-
-
-  const imgItems = images.map((image, index) => {
-    return <Image
-      src={image}
-      key={`image-${index}`}
-      onclick={() => {
-        console.log('clicked index', index);
-        setIndex(index)
-      }}
-    />
-  });
+  }, [images]);
 
   return (
     <>
-      <p>&nbsp;&nbsp;Images ({imgItems.length}) { selectedCount ? ( 
+      <p>&nbsp;&nbsp;Images ({images.length}) { selectedCount ? ( 
         `(${selectedCount} selected)` 
       ) : null }</p>
       <hr />
       <div className='gallery' ref={galleryRef}>
-        {imgItems}
+        <RowsPhotoAlbum 
+          photos={photos} 
+          onClick={({ index }) => {
+            setIndex(index);
+          }}
+        />
       </div>
     </>
 
